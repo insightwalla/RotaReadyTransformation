cb41b1ef8be165d843b76f2c1b0a28d1fae58ed6
insightwalla <127964633+insightwalla@users.noreply.github.com>
2023-10-11 13:51:18 +0100

    updating UI - changed download file name - make it more efficient

--- Transformations.py
+++ Transformations.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import streamlit as st
 import time
+import numpy as np
+
 class TransformationRotaReady:
     def __init__(self, df):
         self.df = df
@@ -16,7 +18,7 @@ class TransformationRotaReady:
         self.df = self.df[self.df["Event type"] == "Shift"]
         # transform all in string
         self.df = self.df.astype(str)
-        st.write('Cleaning done - %s seconds' % round((time.time() - start_time)))
+        st.info('Cleaning done - %s seconds' % round((time.time() - start_time)))
 
     def transformation1(self):
         '''
@@ -41,28 +43,46 @@ class TransformationRotaReady:
         # get min hour from start
         self.min_hour = 4
         # modify end time if end time is less than start time
-        self.df["HourEnd"] = self.df["HourEnd"].apply(lambda x: x+24 if x < self.min_hour else x)
-
+        #self.df["HourEnd"] = self.df["HourEnd"].apply(lambda x: x+24 if x < self.min_hour else x)
+        self.df["HourEnd"] = np.where(self.df["HourEnd"] < self.min_hour, self.df["HourEnd"] + 24, self.df["HourEnd"])
         # get max hour from end
         self.max_hour = self.df["HourEnd"].max()
 
         # create a minute_end column taking only the minute from the end time
         self.df["EndMinutes"] = pd.to_datetime(self.df["Finish"],format = 'mixed', dayfirst=True).dt.minute
         # round the end minutes to 0 - 15 - 30 - 45 minutes
-        self.df["EndMinutes"] = self.df["EndMinutes"].apply(lambda x: 
-                                                            0 if x <= 8 
-                                                            else 15 if x > 8 and x <= 23 
-                                                            else 30 if x > 23 and x <= 38 
-                                                            else 45) 
+        # self.df["EndMinutes"] = self.df["EndMinutes"].apply(lambda x: 
+        #                                                     0 if x <= 8 
+        #                                                     else 15 if x > 8 and x <= 23 
+        #                                                     else 30 if x > 23 and x <= 38 
+        #                                                     else 45) 
+        
+        # import numpy as np
+
+        conditions = [
+            self.df["EndMinutes"] <= 8,
+            (self.df["EndMinutes"] > 8) & (self.df["EndMinutes"] <= 23),
+            (self.df["EndMinutes"] > 23) & (self.df["EndMinutes"] <= 38)
+        ]
+        choices = [0, 15, 30]
+        self.df["EndMinutes"] = np.select(conditions, choices, default=45)
         # create a minute_start column taking only the minute from the start time
         self.df["StartMinutes"] = pd.to_datetime(self.df["Start"],format = 'mixed', dayfirst=True).dt.minute
         # round the start minutes to 0 - 15 - 30 - 45 minutes
-        self.df["StartMinutes"] = self.df["StartMinutes"].apply(lambda x:
-                                                                0 if x <= 8
-                                                                else 15 if x > 8 and x <= 23
-                                                                else 30 if x > 23 and x <= 38
-                                                                else 45)
-        st.write('Transformation 1/4 done - %s seconds' % round((time.time() - start_time)))
+        # self.df["StartMinutes"] = self.df["StartMinutes"].apply(lambda x:
+        #                                                         0 if x <= 8
+        #                                                         else 15 if x > 8 and x <= 23
+        #                                                         else 30 if x > 23 and x <= 38
+        #                                                         else 45)
+        
+        conditions = [
+            self.df["StartMinutes"] <= 8,
+            (self.df["StartMinutes"] > 8) & (self.df["StartMinutes"] <= 23),
+            (self.df["StartMinutes"] > 23) & (self.df["StartMinutes"] <= 38)
+        ]
+        choices = [0, 15, 30]
+        self.df["StartMinutes"] = np.select(conditions, choices, default=45)
+        st.info('Transformation 1/4 done - %s seconds' % round((time.time() - start_time)))
         return self.df
 
     def transformation2(self):
@@ -83,7 +103,6 @@ class TransformationRotaReady:
             #get start hour and end hour
             start_hour = row["HourStart"]
             end_hour = row["HourEnd"]
-            min_start = row["StartMinutes"]
             min_end = row["EndMinutes"]
             for hour_minute in hours_minutes:
                 # check if the hour is between start and end
@@ -95,9 +114,36 @@ class TransformationRotaReady:
                     elif hour_to_populate == end_hour and minute_to_populate <= min_end:
                         self.df.loc[index, hour_minute] = 1 
 
-        st.write('Transformation 2/4 done - %s seconds' % round((time.time() - start_time)))
+        st.info('Transformation 2/4 done - %s seconds' % round((time.time() - start_time)))
         return self.df
 
+    def new_transformation2(self):
+        start_time = time.time()
+        # create a 2D grid of hours and minutes
+        hours = np.arange(self.min_hour, self.max_hour+1)
+        minutes = np.array([0, 15, 30, 45])
+        # reshape the grid into a 1D array of strings
+        hours_minutes = [f"{hour:02d}:{minute:02d}" for hour in hours for minute in minutes]        # create a column for each hour and each 15 minutes
+        self.df[hours_minutes] = 0
+        # populate the columns with 1 if the shift is in that hour and minute
+        for index, row in self.df.iterrows():
+            #get start hour and end hour
+            start_hour = row["HourStart"]
+            end_hour = row["HourEnd"]
+            min_end = row["EndMinutes"]
+            for hour_minute in hours_minutes:
+                # check if the hour is between start and end
+                hour_to_populate = int(hour_minute.split(":")[0])
+                minute_to_populate = int(hour_minute.split(":")[1])
+                if start_hour < end_hour:
+                    if hour_to_populate >= start_hour and hour_to_populate < end_hour:
+                        self.df.loc[index, hour_minute] = 1
+                    elif hour_to_populate == end_hour and minute_to_populate <= min_end:
+                        self.df.loc[index, hour_minute] = 1 
+
+        st.info('Transformation 2/4 done - %s seconds' % round((time.time() - start_time)))
+        return self.df
+    
     def transformation3(self):
         start_time = time.time()
         # here we need to create a column called MinuteAfterClosing (each cafe has a different closing time - relative to day of the week)
@@ -166,7 +212,7 @@ class TransformationRotaReady:
 
         self.df['Closing time'] = self.df.apply(lambda x: res_[x['Site (appointment)']][x['Day of the week']], axis=1)
         #self.data['Closing time'] = self.data['Day of the week'].apply(lambda x: 23 if x in ['Sunday','Monday', 'Tuesday', 'Wednesday'] else 24)
-        st.write('Transformation 3/4 done - %s seconds' % round((time.time() - start_time)))
+        st.info('Transformation 3/4 done - %s seconds' % round((time.time() - start_time)))
         return self.df
     
     def transformation4(self):
@@ -183,13 +229,15 @@ class TransformationRotaReady:
                 return 0
             
         self.df['TotalMinutesAfterClosingTime'] = self.df.apply(lambda_for_time_after_closing, axis=1)
-        st.write('Transformation 4/4 done - %s seconds' % round((time.time() - start_time)))
+        st.info('Transformation 4/4 done - %s seconds' % round((time.time() - start_time)))
         return self.df
     
     def transform(self):
         self.cleaning()
         self.df = self.transformation1()
-        self.df = self.transformation2()
+        self.df = self.new_transformation2()
         self.df = self.transformation3()
         self.df = self.transformation4()
         return self.df
+
+
--- main.py
+++ main.py
@@ -2,28 +2,54 @@ import streamlit as st
 import pandas as pd
 from Transformations import TransformationRotaReady
 
-# upload csv
+
+
+def combine_dfs(dfs):
+    return pd.concat(dfs, ignore_index=True)
+
 st.title('Transforming RotaReady CSV')
-uploaded_file = st.sidebar.file_uploader("Choose a file")
+st.set_page_config(layout="wide")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file", accept_multiple_files=True)
 expander_original = st.expander("Original CSV")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+
+if uploaded_file is not None and uploaded_file != []:
+    if len(uploaded_file) > 1:
+        df = combine_dfs([pd.read_csv(file) for file in uploaded_file])
+    elif len(uploaded_file) == 1:
+        df = pd.read_csv(uploaded_file[0])
+
     # filter only my first name and last name
-    if st.checkbox("Filter only my name"):
-        df = df[(df["First name"] == "Roberto") & (df["Last name"] == "Scalas")]
+    names = df["First name"].unique()
+    
+    choosen_name = st.sidebar.selectbox("Filter by name", names, index = None)
+    if choosen_name is not None:
+        # filter to keep only my name
+        df = df[(df["First name"] == choosen_name)]
+        surnames = df["Last name"].unique()
+
     expander_original.write(df)
 
-    df = TransformationRotaReady(df).df
+    df_transformed = TransformationRotaReady(df).df
     expander_final = st.expander("Final CSV", expanded=True)
-    expander_final.write(df)
+    expander_final.write(df_transformed)
 
+    # get minimum date
+    min_date = df["Start"].min()
+    max_date = df["Start"].max()
+    min_date = pd.to_datetime(min_date).strftime("%Y-%m-%d")
+    max_date = pd.to_datetime(max_date).strftime("%Y-%m-%d")
     # download the csv
     csv = df.to_csv(index=False)
 
     # craete a download button
+
     st.download_button(
         label="Download CSV",
         data=csv,
-        file_name="rota_ready.csv",
+        file_name="rota_ready_{}_{}.csv".format(min_date, max_date),
         mime="text/csv"
     )
+
+else:
+    st.warning("Please upload a CSV file")
